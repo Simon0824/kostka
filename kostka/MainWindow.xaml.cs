8639c804e8d5a1beee4ff463606579fcdb27d221
767Bartek <barteklidzbarski9@gmail.com>
2024-11-14 10:29:35 +0100

    klasa GrawKosci

--- kostka/MainWindow.xaml.cs
+++ kostka/MainWindow.xaml.cs
@@ -11,9 +11,25 @@ using System.Windows.Shapes;
 
 namespace kostka
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    class GrawKosci
+    {
+        public static int ILOSC_KOSCI = 5;
+        public static int ILOSC_SCIAN_KOSCI = 6;
+        public static int ILOSC_NIEZNANA = -1;
+        public static string WYNIK_GRY = "Wynik gry: ";
+        public static string WYNIK_LOSOWANIA = "Wynik tego losowania: ";
+        public static string PLIK_KOSCI_JEDEN = "\\plik1.jpg";
+        public static string PLIK_KOSCI_DWA = "\\plik2.jpg";
+        public static string PLIK_KOSCI_TRZY = "\\plik3.jpg";
+        public static string PLIK_KOSCI_CZTERY = "\\plik4.jpg";
+        public static string PLIK_KOSCI_PIEC = "\\plik5.jpg";
+        public static string PLIK_KOSCI_SZESC = "\\plik6.jpg";
+        public static string PLIK_KOSCI_NIEZNANY = "\\pliknieznany.jpg";
+        private int[] TablicaPowtorzen;
+        public int[] TablicaWynikow;
+        public int iloscPunktow { get; set; };
+        public int iloscPunktowCalaGra { get; set; };
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
