fe926ae73c8633e2bf1d5b7cd43db92ab2dfdff2
Szymon Szostek <szymon.szostek008@gmail.com>
2024-11-14 13:19:48 +0100

    kontuacja

--- kostka/MainWindow.xaml.cs
+++ kostka/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Resources;
 
 namespace kostka
 {
@@ -27,8 +28,80 @@ namespace kostka
         public static string PLIK_KOSCI_NIEZNANY = "\\pliknieznany.jpg";
         private int[] TablicaPowtorzen;
         public int[] TablicaWynikow;
-        public int iloscPunktow { get; set; };
-        public int iloscPunktowCalaGra { get; set; };
+        public int iloscPunktow { get; set; }
+        public int iloscPunktowCalaGra { get; set; }
+
+        public GrawKosci()
+        {
+            iloscPunktow = 0;
+            iloscPunktowCalaGra = 0;
+            TablicaPowtorzen = new int[ILOSC_SCIAN_KOSCI + 1];
+            TablicaWynikow = new int[ILOSC_KOSCI];
+        }
+
+        private int losujpojedynczy()
+        {
+            Random losowa = new Random();
+            return losowa.Next(1, ILOSC_SCIAN_KOSCI + 1);
+        }
+
+        private void obliczwyniki(int[] tablicarzutow)
+        {
+            iloscPunktow = 0;
+            for (int i = 1; i < ILOSC_SCIAN_KOSCI + 1; i++)
+            {
+                if (tablicarzutow[i] > 1)
+                {
+                    iloscPunktow += tablicarzutow[i] = i;
+                }
+            }
+            iloscPunktowCalaGra += iloscPunktow;
+        }
+
+        public void losujWszystko()
+        {
+            for (int i = 0; i < ILOSC_SCIAN_KOSCI; i++)
+            {
+                TablicaPowtorzen[i] = 0;
+            }
+
+            for(int i = 0; i< ILOSC_KOSCI; i++)
+            {
+                TablicaWynikow[i] = losujpojedynczy();
+                TablicaPowtorzen[TablicaWynikow[i]]++;
+            }
+            obliczwyniki(TablicaPowtorzen);
+        }
+
+        public BitmapImage wyswietlObraz(int iloscOczek)
+        {
+            string sciezka;
+            switch (iloscOczek)
+            {
+                case 1:
+                    sciezka = PLIK_KOSCI_JEDEN;
+                    break;
+                case 2:
+                    sciezka = PLIK_KOSCI_DWA;
+                    break;
+                case 3:
+                    sciezka = PLIK_KOSCI_TRZY;
+                    break;
+                case 4:
+                    sciezka = PLIK_KOSCI_CZTERY;
+                    break;
+                case 5:
+                    sciezka = PLIK_KOSCI_PIEC;
+                    break;
+                case 6:
+                    sciezka = PLIK_KOSCI_SZESC;
+                    break;
+                default:
+                    sciezka = PLIK_KOSCI_NIEZNANY;
+                    break;
+            }
+            return;
+        }
     }
     public partial class MainWindow : Window
     {
