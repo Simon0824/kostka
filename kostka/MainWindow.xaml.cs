30d649ccb825e61cb55a57ccf51e24f90539c89f
Simon0824 <szymon.szostek008@gmail.com>
2024-11-21 20:09:51 +0100

    Poprawa działania przycisku resetu oraz dodanie komentarzy

--- kostka/MainWindow.xaml.cs
+++ kostka/MainWindow.xaml.cs
@@ -14,17 +14,17 @@ namespace kostka
 {
     public partial class MainWindow : Window
     {
-        private int ogolnywynik = 0;
+        private int ogolnywynik = 0; //przypisanie ogólnemu wynikowi początkowej wartości 0
         public MainWindow()
         {
             InitializeComponent();
-            int kostkailosc = (int)kostki.Value;
+            int kostkailosc = (int)kostki.Value; //przypisanie ilości kostek oraz ścian wartości z suwaków
             int scianyilosc = (int)sciany.Value;
-            if (kostkailosc == 1 || scianyilosc == 4)
+            if (kostkailosc == 1 || scianyilosc == 4) //jeśli ilość kostek jest 1 lub ilość ścian jest 4 to ma sie pokazać domyślny kwadrat
             {
                 kosteczkiwynik.Children.Add(WyswietlDomyslnyKwadrat());
             }
-            kostki.ValueChanged += (s, e) => ilosckostek.Text = ((int)kostki.Value).ToString();
+            kostki.ValueChanged += (s, e) => ilosckostek.Text = ((int)kostki.Value).ToString(); //przypisanie textblockowi wartości z suwaków
             sciany.ValueChanged += (s, e) => iloscscian.Text = ((int)sciany.Value).ToString();
         }
 
@@ -34,15 +34,15 @@ namespace kostka
             int scianyilosc = (int)sciany.Value;
 
 
-            kosteczkiwynik.Children.Clear();
-            Random random = new Random();
+            kosteczkiwynik.Children.Clear(); 
+            Random random = new Random(); // losowanie kości
             int[] rzuty = new int[kostkailosc];
             for (int i = 0; i < kostkailosc; i++)
             {
-                rzuty[i] = random.Next(1, kostkailosc + 1);
-                kosteczkiwynik.Children.Add(Wyswietlaniezdjec(rzuty[i]));
+                rzuty[i] = random.Next(1, kostkailosc + 1); 
+                kosteczkiwynik.Children.Add(Wyswietlaniezdjec(rzuty[i])); //wyświetlenie zdjęć dla odpowiednich liczb
             }
-            Dictionary<int, int> powtorzenia = new Dictionary<int, int>();
+            Dictionary<int, int> powtorzenia = new Dictionary<int, int>(); //liczenie powtórzeń za pomocą słownika
             foreach (int rzut in rzuty)
             {
                 if (powtorzenia.ContainsKey(rzut))
@@ -50,13 +50,13 @@ namespace kostka
                 else
                     powtorzenia[rzut] = 1;
             }
-            int rzutywynik = 0;
-            foreach (var rzucik in powtorzenia)
+            int rzutywynik = 0; //przypisanie początkowego wyniku rzutu
+            foreach (var rzucik in powtorzenia) //sumowanie powtórzeń
             {
                 if (rzucik.Value > 1)
                     rzutywynik += rzucik.Key * rzucik.Value;
             }
-            wynikrzutu.Text = rzutywynik.ToString();
+            wynikrzutu.Text = rzutywynik.ToString(); //liczenie punktów rzutu oraz ogólnych
             ogolnywynik += rzutywynik;
             wynikogolny.Text = ogolnywynik.ToString();
 
@@ -64,14 +64,15 @@ namespace kostka
 
         private void ResetGry(object sender, RoutedEventArgs e)
         {
-            wynikrzutu.Text = "0";
+            wynikrzutu.Text = "0"; //resetowanie wyników oraz wartości
             wynikogolny.Text = "0";
             kostki.Value = 0;
             sciany.Value = 0;
             kosteczkiwynik.Children.Clear();
+            kosteczkiwynik.Children.Add(WyswietlDomyslnyKwadrat());
         }
 
-        private Image Wyswietlaniezdjec(int wartosc)
+        private Image Wyswietlaniezdjec(int wartosc) //tworzenie obrazu dla konkretnych wyników
         {
                 return new Image
                 {
@@ -81,7 +82,7 @@ namespace kostka
                     Margin = new Thickness(5)
                 };
         }
-        private Image WyswietlDomyslnyKwadrat()
+        private Image WyswietlDomyslnyKwadrat() //tworzenie obrazu gdy wartość jest jeszcze nie ustawiona
         {
             return new Image
             {
